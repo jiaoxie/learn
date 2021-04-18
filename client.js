7b4935d2d2b44a0c39732f754b58a48425ae2b82
jiao.xie <949238974@qq.com>
2021-04-18 23:55:48 +0800

    update

--- client.js
+++ client.js
@@ -76,7 +76,48 @@ class ResponseParser{
         }
     }
     receiveChar(char) {
-
+        if(this.current === this.WAITING_STATUS_LINE) {
+            if (char === '\r') {
+                this.current = this.WAITING_STATUS_LINE_END
+            } else {
+                this.statusLine += char
+            }
+        } else if(this.current === this.WAITING_STATUS_LINE_END){
+            if (char === '\n') {
+                this.current = this.WAITING_HEADER_NAME
+            }
+        } else if(this.current === this.WAITING_HEADER_NAME){
+            if(char === ':'){
+                this.current = this.WAITING_HEADER_SPACE
+            } else if(char === '\r') {
+                this.current = this.WAITING_HEADER_BLOCK_END
+            } else {
+                this.headerName +=char
+            }
+        } else if (this.current === this.WAITING_HEADER_SPACE) {
+            if (char === ' ') {
+                this.current = this.WAITING_HEADER_LINE_VALUE
+            }
+        } else if(this.current === this.WAITING_HEADER_VALUE){
+            if(char ==='\r') {
+                this.current = this.WAITING_HEADER_LINE_END
+                this.headers[this.headerName] = this.headerValue
+                this.headerName = ''
+                this.headerValue = ''
+            } else {
+                this.headerValue += char
+            }
+        } else if(this.current === this.WAITING_HEADER_LINE_END){
+            if(char === '\n') {
+                this.current = this.WAITING_HEADER_NAME
+            }
+        } else if(this.current === this.WAITING_HEADER_BLOCK_END){
+            if(char === '\n'){
+                this.current = this.WAITING_BODY
+            }
+        } else if(this.current === this.WAITING_BODY){
+            console.log(char)
+        }
     }
 }
 void async function(){
